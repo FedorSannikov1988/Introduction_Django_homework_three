56a2c346db4656aa91f2f2942c34229a498e8a50
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2024-01-16 21:45:05 +0300

    finished job

--- educational_online_store/shop/urls.py
+++ educational_online_store/shop/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import get_all_customer_orders_and_lists_items_in_order, \
-                   get_list_products_ordered_by_customer_from_all_his_orders
+                   get_list_products_ordered_by_customer_from_all_his_orders, \
+                   GetListProductsOrderedByCustomerIdFromAllHisOrders, \
+                   GetListProductsOrderedByCustomerIdFromAllHisOrdersV2
 
 
 urlpatterns = [
@@ -13,4 +15,13 @@ urlpatterns = [
          get_list_products_ordered_by_customer_from_all_his_orders,
          name='get_list_products_ordered_by_customer_from_all_his_orders'),
 
+    #только для того что бы поработать с классами вариант №2:
+    path('get_list_products_ordered_by_customer_from_all_his_orders_v2/<int:id_client>/',
+         GetListProductsOrderedByCustomerIdFromAllHisOrders.as_view(),
+         name='get_list_products_ordered_by_customer_from_all_his_orders_v2'),
+
+    #только для того что бы поработать с классами вариант №3:
+    path('get_list_products_ordered_by_customer_from_all_his_orders_v3/<int:id_client>/',
+         GetListProductsOrderedByCustomerIdFromAllHisOrdersV2.as_view(),
+         name='get_list_products_ordered_by_customer_from_all_his_orders_v3'),
 ]
--- educational_online_store/shop/views.py
+++ educational_online_store/shop/views.py
@@ -1,5 +1,7 @@
+from django.views import View
 from shop.models import Client, Order
 from datetime import datetime, timedelta
+from django.views.generic import TemplateView
 from dateutil.relativedelta import relativedelta
 from django.shortcuts import render, get_object_or_404
 
@@ -96,6 +98,134 @@ def get_list_products_ordered_by_customer_from_all_his_orders(request,
     return render(request,
                   "get_list_products_ordered_by_customer_from_all_his_orders.html",
                   context)
+
+
+class GetListProductsOrderedByCustomerIdFromAllHisOrders(View):
+
+    def get(self, request, id_client: int):
+
+        client = get_object_or_404(Client, pk=id_client)
+
+        orders = \
+            Order.objects.filter(client=client).all()
+
+        current_datetime = datetime.now()
+
+        seven_days_ago = current_datetime - timedelta(days=7)
+        thirty_days_ago = current_datetime - timedelta(days=30)
+        year_days_ago_v2 = current_datetime - relativedelta(years=1)
+
+        seven_days_ago_list_product: list = []
+        thirty_days_ago_list_product: list = []
+        year_ago_list_product: list = []
+
+        for order in orders:
+            date_and_time_placing_order: datetime = \
+                order.date_and_time_placing_order.replace(tzinfo=None)
+
+            for_sort_products_in_ordered(list_products=
+                                         seven_days_ago_list_product,
+                                         date_and_time_placing_order=
+                                         date_and_time_placing_order,
+                                         what_is_it_compared_to=
+                                         seven_days_ago,
+                                         order=
+                                         order)
+
+            for_sort_products_in_ordered(list_products=
+                                         thirty_days_ago_list_product,
+                                         date_and_time_placing_order=
+                                         date_and_time_placing_order,
+                                         what_is_it_compared_to=
+                                         thirty_days_ago,
+                                         order=
+                                         order)
+
+            for_sort_products_in_ordered(list_products=
+                                         year_ago_list_product,
+                                         date_and_time_placing_order=
+                                         date_and_time_placing_order,
+                                         what_is_it_compared_to=
+                                         year_days_ago_v2,
+                                         order=
+                                         order)
+
+        context = {
+            "name_client": client.name,
+            "products": [
+                seven_days_ago_list_product,
+                thirty_days_ago_list_product,
+                year_ago_list_product
+            ]
+        }
+
+        return render(request,
+                      "get_list_products_ordered_by_customer_from_all_his_orders.html",
+                      context)
+
+
+class GetListProductsOrderedByCustomerIdFromAllHisOrdersV2(TemplateView):
+    template_name = "get_list_products_ordered_by_customer_from_all_his_orders.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        id_client = self.kwargs['id_client']
+
+        client = get_object_or_404(Client, pk=id_client)
+
+        orders = \
+            Order.objects.filter(client=client).all()
+
+        current_datetime = datetime.now()
+
+        seven_days_ago = current_datetime - timedelta(days=7)
+        thirty_days_ago = current_datetime - timedelta(days=30)
+        year_days_ago_v2 = current_datetime - relativedelta(years=1)
+
+        seven_days_ago_list_product: list = []
+        thirty_days_ago_list_product: list = []
+        year_ago_list_product: list = []
+
+        for order in orders:
+            date_and_time_placing_order: datetime = \
+                order.date_and_time_placing_order.replace(tzinfo=None)
+
+            for_sort_products_in_ordered(list_products=
+                                         seven_days_ago_list_product,
+                                         date_and_time_placing_order=
+                                         date_and_time_placing_order,
+                                         what_is_it_compared_to=
+                                         seven_days_ago,
+                                         order=
+                                         order)
+
+            for_sort_products_in_ordered(list_products=
+                                         thirty_days_ago_list_product,
+                                         date_and_time_placing_order=
+                                         date_and_time_placing_order,
+                                         what_is_it_compared_to=
+                                         thirty_days_ago,
+                                         order=
+                                         order)
+
+            for_sort_products_in_ordered(list_products=
+                                         year_ago_list_product,
+                                         date_and_time_placing_order=
+                                         date_and_time_placing_order,
+                                         what_is_it_compared_to=
+                                         year_days_ago_v2,
+                                         order=
+                                         order)
+
+        context['name_client'] = client.name
+        context['products'] = [
+            seven_days_ago_list_product,
+            thirty_days_ago_list_product,
+            year_ago_list_product
+        ]
+        return context
 
 
 def page_not_found(request, exception):
